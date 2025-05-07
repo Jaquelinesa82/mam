f00c2dbf08681279807646690fe39a85993b7c6f
jaqueline <jaquelinesa.82@gmail.com>
2025-05-07 14:24:26 -0300

    Input sanitization

--- appointments/serializers.py
+++ appointments/serializers.py
@@ -1,8 +1,20 @@
 from rest_framework import serializers
 from appointments.models import Appointment
+from datetime import datetime
+from professionals.models import Professionals
 
 
 class AppointmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Appointment
-        fields = '__all__'
+        fields = ('id', 'date', 'professional')
+        
+    def validate_date(self, value):
+        if value < datetime.now():
+            raise serializers.ValidationError('A data da consulta deve ser no futuro')
+        return value
+    
+    def validate_professional(self, value):
+        if not Professionals.objects.filter(pk=value.pk).exists():
+            raise serializers.ValidationError('Profissional não encontrado.')
+        return value
--- professionals/serializers.py
+++ professionals/serializers.py
@@ -8,3 +8,20 @@ class ProfessionalSerializer(serializers.ModelSerializer):
         fields = (
             'id', 'name', 'profession', 'address', 'contact'
         )
+        
+    def validate_name(self, value):
+        return value.strip().title()
+    
+    def validate_profession(self, value):
+        return value.strip().title()
+    
+    def validate_address(self, value):
+        return value.strip()
+    
+    def validate_contact(self, value):
+        cleaned = value.strip().replace(" ", "").replace("-", "")
+        if not cleaned.isdigit():
+            raise serializers.ValidationError('O contato deve conter apenas números')
+        if len(cleaned) < 10:
+            raise serializers.ValidateError('O contato deve ter pelo menos 10 dígitos.')
+        return cleaned
