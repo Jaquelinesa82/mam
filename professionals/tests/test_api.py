fbc7b427e9b4cea939e3b36e94faea1a008d3bde
jaqueline <jaquelinesa.82@gmail.com>
2025-05-07 21:39:20 -0300

    added test

---
+++ professionals/tests/test_api.py
@@ -0,0 +1,26 @@
+import pytest
+from rest_framework.test import APIClient
+from faker import Faker
+
+@pytest.mark.django_db()
+def test_register_professional_return_201_created():
+    client = APIClient()
+    faker = Faker()
+    
+    name = faker.name()
+    profession = faker.job()
+    address = faker.address()
+    contact = faker.phone_number()
+
+    payload = {
+        'name' : name,
+        'profession' : profession,
+        'address' : address,
+        'contact' : contact,
+    }
+    
+    response = client.post('/api/professionals/register_professional/', payload, format='json')
+    
+    assert response.status_code == 201
+    assert response.data['name'] == payload['name']
+    
